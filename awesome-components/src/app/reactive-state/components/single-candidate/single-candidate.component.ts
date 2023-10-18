db6082c4ef8cb81f6b169d71bc2d200af82524ef
JeremyPromsy <jeremypromsy@gmail.com>
2023-10-18 14:57:30 +0200

    Avancement

---
+++ awesome-components/src/app/reactive-state/components/single-candidate/single-candidate.component.ts
@@ -0,0 +1,46 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, switchMap, take, tap } from 'rxjs';
+import { Candidate } from '../../models/candidate.model';
+import { CandidatesService } from '../services/candidate.service';
+
+@Component({
+  selector: 'app-single-candidate',
+  templateUrl: './single-candidate.component.html',
+  styleUrls: ['./single-candidate.component.scss'], 
+  changeDetection: ChangeDetectionStrategy.OnPush, 
+})
+export class SingleCandidateComponent implements OnInit{
+
+  loading$!: Observable<boolean>;
+  candidate$!: Observable<Candidate>;
+
+constructor(private candidatesService: CandidatesService,
+            private route: ActivatedRoute) { }
+
+ngOnInit(): void {
+    this.initObservables();
+}
+
+private initObservables() {
+    this.loading$ = this.candidatesService.loading$;
+    this.candidate$ = this.route.params.pipe(
+      switchMap(params => this.candidatesService.getCandidateById(+params['id']))
+  );
+}
+
+onHire() {}
+
+onRefuse() {
+  this.candidate$.pipe(
+      take(1),
+      tap(candidate => {
+          this.candidatesService.refuseCandidate(candidate.id);
+          this.onGoBack();
+      })
+  ).subscribe();
+}
+
+onGoBack() {}
+
+}
